234df0156a73f38d0ac524b5cde6367eddf03273
Elise Reppond <elise.reppond@gmail.com>
2018-05-11 11:04:30 -0700

    new recommender implemented - no debugging yet

--- src/clustering.py
+++ src/clustering.py
@@ -1,31 +1,78 @@
 import pandas as pd
-
 from sklearn.cluster import KMeans
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+class Recommending:
+    def __init__(self, n_clusters=30):
+        '''Initializes the TFIDF Vectorizer and KMeans Obj'''
+
+        self.n_clusters = n_clusters
+        self.tfidf = TfidfVectorizer(stop_words='english', max_features=200)
+        self.km = KMeans(n_clusters=self.n_clusters)
+        self.results = {}
 
 
+    def fit_transform(self, X):
+        '''Fits and transforms TFIDF and fits KMeans.
 
-class Clustering:
-	def __init__(self, n_clusters=60):
-		'''Initializes the TFIDF Vectorizer and KMeans Obj'''
+        Params:
+            X (array): Array of the descriptions of houses
 
-		self.n_clusters = n_clusters
-		self.tfidf = TfidfVectorizer(stop_words='english', max_features=1000)
-		# self.km = KMeans(n_clusters=self.n_clusters)
+        '''
+        self.tfidf.fit(X)
+        desc_tfidf = self.tfidf.transform(X)
+        self.km.fit(desc_tfidf.todense())
+        return desc_tfidf
 
+    def cosine_sim(self,tfidf):
+        ''' Creates a dictionary of the houses to consider based on cosine similarity.
+        
+        Params:
+            tfidf (Tfidf object): object created in fit method
+        '''
+        
+        cosine_similarities = cosine_similarity(tfidf,tfidf)
+        for idx, row in df.iterrows():
+            if idx < 2822:
+                similar_indices = cosine_similarities[idx].argsort()[:-5:-1]
+                similar_items = [(cosine_similarities[idx][i], i) for i in similar_indices]
+                self.results[row['ID']] = similar_items[1:]
+                
+    def item(id):
+        ''' Helper method for returning item in dataframe when looking for recommendations.
+        
+        Params:
+            id (int): id of recommended house
+        '''
+        
+        return df.loc[df.index == id][['ADDRESS', 'URL']].tolist()[0]
 
-	def fit_transform(self, X):
-		'''Fits and transforms TFIDF and fits KMeans.
-		
-		Params:
-			X (array): Array of the descriptions of houses
+    
+    def recommend(id, num):
+        ''' Prints the recommendations for that house.
+        
+        Params:
+            id (int): id of house that needs recommendations
+            num (int): num of recommendations
+        '''
+        
+        try:
+            if (num == 0):
+                print("Unable to recommend any house")
+            elif (num==1):
+                print("Recommending " + str(num) + " house similar to " + item(id))
+            else :
+                print("Recommending " + str(num) + " houses similar to " + item(id))
 
-		'''
-		self.tfidf.fit(X)
-		desc_tfidf = self.tfidf.transform(X)
-		return desc_tfidf
-		# print(desc_tfidf.shape)
-		# self.km.fit(desc_tfidf.todense())
+            print("----------------------------------------------------------")
+            recs = results[id][:num]
+            for rec in recs:
+                print("You may also like to look at: " + item(rec[1]) + " (score:" + str(rec[0]) + ")")
+        except IndexError:
+            print('These are the houses most similar to your house')
+            pass
+
 
 
 	def result(self, df):
@@ -72,7 +119,7 @@ def get_data(file, fave_file=None):
 	Returns:
 		df (DataFrame): pandas dataframe of data from file
 	'''
-	
+
 	df = pd.read_csv(file)
 	df['FAVORITE'] = 'N'
 	if fave_file != None:
