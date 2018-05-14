16ec021e2aa87df1e3a972b389a36e11d2ded45e
Elise Reppond <elise.reppond@gmail.com>
2018-05-14 12:16:13 -0700

    scraping data fixed

--- src/update_data.py
+++ src/update_data.py
@@ -8,111 +8,114 @@ from datetime import datetime
 
 class Data_Update:
 
-	def __init__(self, old_data):
-		'''Initializes the self parameters.
+    def __init__(self, old_data):
+        '''Initializes the self parameters.
 
-		Params:
-			old_data (filename): name of csv file for old data
-			new_data (filename): name of csv file for new data
-		'''
+        Params:
+            old_data (filename): name of csv file for old data
+            new_data (filename): name of csv file for new data
+        '''
 
-		self.df_old_data = pd.read_csv(old_data)
-		self.df_old_data = self.df_old_data.rename(columns=
-			{'URL (SEE http://www.redfin.com/buy-a-home/comparative-\
-			market-analysis FOR INFO ON PRICING)': 'URL'})
-		self.df_new_data = pd.DataFrame()
-
-
-	def collect_new_data(self):
-		'''Calls the webscraper that downloads necessary datasets.
-		'''
-
-		browser = Chrome()
-		for idx, zipcode in enumerate(self.df_old_data['ZIP'].unique()):
-			try:
-				browser.get('https://www.redfin.com/zipcode/{}'.
-					format(zipcode))
-				time.sleep(1 + random.random() * 3)
-				download_button = browser.find_element_by_css_selector(
-					'a#download-and-save')
-				download_button.click()
-			except:
-				pass
-
-	def collecting_files(self):
-		''' Collects the files from the Downloads folder and adds 
-		them to the dataframe.
-		'''
-
-		now = datetime.now()
-		list_of_files = []
-		directory = os.fsencode('/Users/elisereppond/Downloads')
-		for file in os.listdir(directory):
-			filename = os.fsdecode(file)
-			month = 5
-			day = 9
-			if month <= 9:
-				month = '0{}'.format(month)
-			if day <= 9:
-				day = '0{}'.format(day)
-			if filename.startswith('redfin_{}-{}-{}'.format(now.year, 
-				month, day)):
-				list_of_files.append(pd.read_csv('/Users/elisereppond/Downloads/{}'.\
-					format(filename)))
-		try: 
-			self.df_new_data = pd.concat(list_of_files)
-		except ValueError:
-			self.df_new_data = self.df_old_data
-		self.df_new_data = self.clean_data()
+        self.df_old_data = pd.read_csv(old_data)
+        self.df_old_data = self.df_old_data.rename(columns=
+            {'URL (SEE http://www.redfin.com/buy-a-home/comparative-\
+            market-analysis FOR INFO ON PRICING)': 'URL'})
+        self.df_new_data = pd.DataFrame()
 
 
-	def clean_data(self):
-		''' Cleans the data to match the proper format necessary for 
-		modeling.
-		'''
+    def collect_new_data(self):
+        '''Calls the webscraper that downloads necessary datasets.
+        '''
 
-		self.df_new_data['DESC'] = 'No Description'
-		self.df_new_data = self.df_new_data.rename(columns=
-			{'URL (SEE http://www.redfin.com/buy-a-home/comparative-market-analysis FOR INFO ON PRICING)': 
-			'URL', 
-			'$/SQUARE FEET': 'PRICE/SQUAREFT'})
-		self.df_new_data['SALE TYPE'] = self.df_new_data['SALE TYPE'].apply(
-			lambda x: 1 if x == 'MLS Listing' or x == 'For-Sale-by-Owner Listing' 
-			else 0)
-		self.df_new_data.drop(self.df_new_data[self.df_new_data['STATE']
-			!= 'WA'].index, inplace=True)
-		self.df_new_data['LABEL'] = 0
-		return self.df_new_data
+        browser = Chrome()
+        for idx, zipcode in enumerate(self.df_old_data['ZIP'].unique()):
+            try:
+                browser.get('https://www.redfin.com/zipcode/{}'.
+                    format(zipcode))
+                time.sleep(1 + random.random() * 3)
+                download_button = browser.find_element_by_css_selector(
+                    'a#download-and-save')
+                download_button.click()
+            except:
+                pass
+
+    def collecting_files(self):
+        ''' Collects the files from the Downloads folder and adds 
+        them to the dataframe.
+        '''
+
+        now = datetime.now()
+        list_of_files = []
+        directory = os.fsencode('/Users/elisereppond/Downloads')
+        for file in os.listdir(directory):
+            filename = os.fsdecode(file)
+            month = 5
+            day = 9
+            if month <= 9:
+                month = '0{}'.format(month)
+            if day <= 9:
+                day = '0{}'.format(day)
+            if filename.startswith('redfin_{}-{}-{}'.format(now.year, 
+                month, day)):
+                list_of_files.append(pd.read_csv('/Users/elisereppond/Downloads/{}'.\
+                    format(filename)))
+        try: 
+            self.df_new_data = pd.concat(list_of_files)
+        except ValueError:
+            self.df_new_data = self.df_old_data
+        self.df_new_data = self.clean_data()
+        self.df_new_data = self.df_new_data.reset_index().drop('index', axis=1)
 
 
-	def compare_datasets(self):
-		''' Adds the descriptions to the new data from the 
-		old data where the address is the same.
-		'''
+    def clean_data(self):
+        ''' Cleans the data to match the proper format necessary for 
+        modeling.
+        '''
 
-		for idx_old, old_row in self.df_old_data.iterrows():
-			for idx_new, new_row in self.df_new_data.iterrows():
-				if (old_row['ADDRESS'] == new_row['ADDRESS'] and 
-					old_row['DESC'] not in self.df_new_data['DESC']): 
-					print('adding existing desc')                   
-					self.df_new_data.loc[idx_new,'DESC'] = old_row['DESC']
+        self.df_new_data['DESC'] = 'No Description'
+        self.df_new_data = self.df_new_data.rename(columns=
+            {'URL (SEE http://www.redfin.com/buy-a-home/comparative-market-analysis FOR INFO ON PRICING)': 
+            'URL', 
+            '$/SQUARE FEET': 'PRICE/SQUAREFT'})
+        self.df_new_data['SALE TYPE'] = self.df_new_data['SALE TYPE'].apply(
+            lambda x: 1 if x == 'MLS Listing' or x == 'For-Sale-by-Owner Listing' 
+            else 0)
+        self.df_new_data.drop(self.df_new_data[self.df_new_data['STATE']
+            != 'WA'].index, inplace=True)
+        self.df_new_data['LABEL'] = 0
+        return self.df_new_data
 
 
-	def scraping_desc(self):
-		''' Scrapes descriptions for descriptions of houses recently 
-		added to data.
-		'''
+    def compare_datasets(self):
+        ''' Adds the descriptions to the new data from the 
+        old data where the address is the same.
+        '''
 
-		for idx, row in self.df_new_data.iterrows():
-			if len(row['DESC']) < 16:
-				url = row['URL']
-				try:
-					browser.get(url)
-					sel = "div.sectionContent"
-					description = browser.find_elements_by_css_selector(sel)
-					self.df_new_data.loc[idx,'DESC'] = description
-				except:
-					self.df_new_data.loc[idx,'DESC'] = 'No Description'
+        for idx_new, new_row in self.df_new_data.iterrows():
+            if str(new_row['ADDRESS']) in np.array(self.df_old_data['ADDRESS']):
+                ind = self.df_old_data.index[self.df_old_data['ADDRESS'] == str(new_row['ADDRESS'])].tolist()
+                for i in ind:
+                    self.df_new_data.loc[idx_new,'DESC'] = self.df_old_data.loc[i, 'DESC']
+
+
+    def scraping_desc(self):
+        ''' Scrapes descriptions for descriptions of houses recently 
+        added to data.
+        '''
+
+        browser = Chrome()
+        for idx, row in self.df_new_data.iterrows():
+            desc = str(row['DESC'])
+            length = len(desc)
+            if length < 15:
+                url = row['URL']
+                try:
+                    browser.get(url)
+                    sel = "div.sectionContent"
+                    description = browser.find_elements_by_css_selector(sel)
+                    self.df_new_data.loc[idx,'DESC'] = description
+                except:
+                    self.df_new_data.loc[idx,'DESC'] = 'No Description'
 
 	def replace_old_csv(self):
 		''' Replaces the old csv files with the updated ones.'''
