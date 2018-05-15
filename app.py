3a1a3abd2029ba4b1babdfbf2f0b614839cdd023
Elise Reppond <elise.reppond@gmail.com>
2018-05-15 12:35:22 -0700

    basics working for welcome, favorites, and homepage

---
+++ app.py
@@ -0,0 +1,48 @@
+"""
+Simple flask app.
+"""
+from flask import (Flask,
+                   jsonify,
+                   render_template,
+                   request, 
+                   url_for)
+# from ec2.prophet_db import web_query
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    return render_template('index.html')
+
+@app.route('/welcome', methods=['GET', 'POST'])
+def welcome():
+#    if request.method == 'GET':
+        # do stuff when the form is submitted
+
+        # redirect to end the POST handling
+        # the redirect can be to the same route or somewhere else
+#        return redirect(url_for('index'))
+
+    # show the form, it wasn't submitted
+    return render_template('welcome.html')
+
+@app.route('/favorites', methods=['GET', 'POST'])
+def favorites():
+#    if request.method == 'GET':
+        # do stuff when the form is submitted
+
+        # redirect to end the POST handling
+        # the redirect can be to the same route or somewhere else
+#        return redirect(url_for('index'))
+
+    # show the form, it wasn't submitted
+    return render_template('favorites.html')
+
+
+def main():
+    app.run(host='0.0.0.0', port=5000, debug=True)
+
+
+if __name__ == '__main__':
+    main()
