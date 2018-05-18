a75f48a5d72211dc3712f84765ba8cf52c288df3
Elise Reppond <elise.reppond@gmail.com>
2018-05-18 09:19:04 -0700

    order of functions changed to make the file more readable

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 """
-Simple flask app.
+* Note: pages ordered by what appears on the navigation bar at the top of the website
 """
 from flask import (Flask,
                    jsonify,
@@ -18,13 +18,27 @@ from src.clustering import do_everything
 app = Flask(__name__)
 
 
-@app.route('/recommendations', methods=['GET', 'POST'])
-def recommendations():
-    return render_template('recommendations.html') 
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    '''Main page of website that gives access to the rest of the attributes of the page.
+
+    Returns:
+        index.html (html template): main page template
+    '''
+
+    if request.method == 'POST':
+        return redirect(url_for('uploadajax'))
+    return render_template('index.html')
 
 
 @app.route('/uploadajax', methods=['POST'])
 def uploadajax():
+    '''File that is processed in the ajax function at the beginning of the recommendations page.
+
+    Returns: 
+        uploaded_file.html (html template): template for what is displayed once the uploaded file has been processed.
+    '''
+
     file = request.files['file']
     f = BytesIO()
     file.save(f)
@@ -35,27 +49,38 @@ def uploadajax():
     return render_template('uploaded_file.html', data=zip(*list_of_vals), columns=columns)
 
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    if request.method == 'POST':
-        return redirect(url_for('uploadajax'))
-    return render_template('index.html')
+@app.route('/data', methods=['GET', 'POST'])
+def data():
+    '''Data page to display what the data looks like on the end of the recommender system.
+    
+    * Note: this function calls a function 'do_everything' from clustering.py that does 
+        everything in order to make the recommender system run.
 
-
-@app.route('/table')
-def table():
+    Returns:
+        
+    '''
     df = pd.read_csv('data/final_html.csv')
     df.drop('Unnamed: 0', axis =1, inplace=True)
     df.fillna(0, inplace=True)
     list_of_vals = [list(df[i].values) for i in df]
     columns = df.columns
-    return render_template('tabulator-table.html')
+    return render_template('data.html', data=zip(*list_of_vals), columns=columns)
+
+@app.route('/favorites', methods=['GET', 'POST'])
+def favorites():
+    # Favorites page to explain how to get the favorited csv file
+    return render_template('favorites.html')
+
+
+@app.route('/recommendations', methods=['GET', 'POST'])
+def recommendations():
+    return render_template('recommendations.html') 
+
 
 @app.route('/uploaded_file', methods = ['GET', 'POST'])
 def uploaded_file():
     #for uploading file 
     return render_template('uploaded_file.html')
-
 
 
 @app.route('/welcome', methods=['GET', 'POST'])
@@ -64,21 +89,16 @@ def welcome():
     return render_template('welcome.html')
 
 
-@app.route('/favorites', methods=['GET', 'POST'])
-def favorites():
-    # Favorites page to explain how to get the favorited csv file
-    return render_template('favorites.html')
+# @app.route('/table')
+# def table():
+#     df = pd.read_csv('data/final_html.csv')
+#     df.drop('Unnamed: 0', axis =1, inplace=True)
+#     df.fillna(0, inplace=True)
+#     list_of_vals = [list(df[i].values) for i in df]
+#     columns = df.columns
+#     return render_template('tabulator-table.html')
 
 
-@app.route('/data', methods=['GET', 'POST'])
-def data():
-    # Data page to show what the data looks like
-    df = pd.read_csv('data/final_html.csv')
-    df.drop('Unnamed: 0', axis =1, inplace=True)
-    df.fillna(0, inplace=True)
-    list_of_vals = [list(df[i].values) for i in df]
-    columns = df.columns
-    return render_template('data.html', data=zip(*list_of_vals), columns=columns)
 
 
 if __name__ == '__main__':
